9b09da412f64052e29accd88c96cfd57cf87e043
Shaun Wu <Alexbay218@gmail.com>
2018-05-18 13:11:50 -0400

    Added initial video capture

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-import cv2
+import numpy as np
+import cv2 as cv
+import sys
 
+if(len(sys.argv) > 1):
+    cap = cv.VideoCapture(sys.argv[1])
+else:
+    cap = cv.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv.imshow('frame',frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
