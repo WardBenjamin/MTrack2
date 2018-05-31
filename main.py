6b7a56086af5c612ca0211e59c6da296748333ea
Shaun Wu <Alexbay218@gmail.com>
2018-05-31 08:04:27 -0400

    Vector of velocity and graphing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cv2 as cv
 import sys
 import time
@@ -22,6 +23,11 @@ cv.namedWindow("frame")
 # Create a new window for trackbars
 cv.namedWindow("bars", cv.WINDOW_NORMAL)
 
+cv.namedWindow("graph", cv.WINDOW_NORMAL)
+cv.resizeWindow("graph", 500, 500)
+
+blank = np.zeros((256, 256, 1), dtype = "uint8")
+
 # These values are used to filter the camera image so that H is between Hmin and Hmax, S is between Smin and Smax, and
 # V is between Vmin and Vmax.  If the HSV value falls outside of any of the ranges, then it is set to 0 (a black pixel).
 # This is how we can filter for the color of the tracked object.
@@ -37,6 +43,7 @@ def on_win_click(event, x, y, flags, params):
     global raw_x
     global raw_y
     global time_offset
+    global data
     if event == cv.EVENT_LBUTTONUP:
         if current_point >= len(points) - 1:
             current_point = 0
@@ -47,6 +54,7 @@ def on_win_click(event, x, y, flags, params):
     raw_y = y
     if event == cv.EVENT_RBUTTONUP:
         time_offset = time.clock()
+        data = []
 
 # These values are used to define the 2d plane mapped to pixel coordinates
 points = [1, 0, 2, 0, 0, 1, 0, 2]
@@ -70,6 +78,14 @@ cv.createTrackbar("Y Scale", "bars", 1000, 1000, no_op)
 
 cv.setMouseCallback("frame", on_win_click, )
 
+data = []
+raw_xprev = 0
+raw_yprev = 0
+vy = 0
+vx = 0
+ax = 0
+ay = 0
+
 time_offset = time.clock();
 while True:
     # image is pulled from the camera using OpenCV. This variable represents all of the pixel values coming off the
@@ -82,6 +98,7 @@ while True:
     #
     # ret is a True or False value if the camera read was successful (unused)
     ret, frame = cap.read()
+    blank = np.zeros((500, 500, 3), dtype = "uint8")
 
     # Our operations on the frame come here
     image, masked, thresholded, path = process(frame)
@@ -90,7 +107,7 @@ while True:
         raw_y = path[-1][1]
 
     # Drawing lines to indicate the xy coord plane
-    drawn_frame = image
+    drawn_frame = frame
     cv.arrowedLine(drawn_frame, (points[0], points[1]), (points[2], points[3]), (255, 0, 0), 5)
     cv.arrowedLine(drawn_frame, (points[4], points[5]), (points[6], points[7]), (0, 0, 255), 5)
     cv.circle(drawn_frame, (points[0], points[1]), 5, (255, 0, 0), -1)
@@ -108,11 +125,56 @@ while True:
     # Processing data
     scaled_data = compute(points, org_data, [raw_x, raw_y, scales[0], scales[1]])
     scaled_data[2] = time.clock() - time_offset
+    data.append(scaled_data)
+    while len(data) > 100:
+        data.pop(0)
+    if len(data) > 5 and (not ((data[len(data)-2][2] - data[len(data)-1][2]) == 0)):
+        #ax = ((raw_xprev - raw_x)/(data[len(data)-2][2] - data[len(data)-1][2])-vx)/(data[len(data)-2][2] - data[len(data)-1][2])
+        #ay = ((raw_yprev - raw_y)/(data[len(data)-2][2] - data[len(data)-1][2])-vy)/(data[len(data)-2][2] - data[len(data)-1][2])
+        vx = (raw_xprev - raw_x)/(data[len(data)-2][2] - data[len(data)-1][2])
+        vy = (raw_yprev - raw_y)/(data[len(data)-2][2] - data[len(data)-1][2])
+    raw_xprev = raw_x
+    raw_yprev = raw_y
+    
+    cv.arrowedLine(drawn_frame, (int(raw_x), int(raw_y)),(int(raw_x+vx), int(raw_y+vy)), (255, 255, 0), 5)
+    b = 0
+    max_x = data[0][0]
+    max_y = data[0][1]
+    max_t = data[0][2]
+    while b < len(data):
+        if data[b][0] > max_x:
+            max_x = data[b][0]
+        if data[b][1] > max_x:
+            max_x = data[b][1]
+        if data[b][2] > max_t:
+            max_t = data[b][2]
+        b += 1
+    b = 0
+    if max_x > 0:
+        max_x = 500/max_x
+    if max_y > 0:
+        max_y = 500/max_y
+    if max_t > 0:
+        max_t = 500/max_t
+    while b < (len(data) - 1):
+        if int(500-data[b+1][0]*max_x) == 0 and not (int(500-data[b][0]*max_x) == 0):
+            cv.line(blank, (int(b*5),int(500-data[b][0]*max_x)),(int(b*5),int(500-data[b][0]*max_x)), (255,0,0),5)
+        else:
+            cv.line(blank, (int(b*5),int(500-data[b][0]*max_x)),(int(b*5),int(500-data[b+1][0]*max_x)), (255,0,0),5)
+        if int(500-data[b+1][1]*max_x) == 0 and not (int(500-data[b][1]*max_x) == 0):
+            cv.line(blank, (int(b*5),int(500-data[b][1]*max_x)),(int(b*5),int(500-data[b][1]*max_x)), (0,0,255),5)
+        else:
+            cv.line(blank, (int(b*5),int(500-data[b][1]*max_x)),(int(b*5),int(500-data[b+1][1]*max_x)), (0,0,255),5)
+        b += 1
+    #cv.arrowedLine(drawn_frame, (int(raw_x), int(raw_y)),(int(raw_x+ax), int(raw_y+ay)), (0, 255, 255), 5)
+    #print(str(vx) + "," + str(vy))
+    # print(scaled_data)
 
     # Display the resulting frame
     cv.imshow("frame", drawn_frame)
-    cv.imshow("mask", masked)
+    cv.imshow("mask", image)
     cv.imshow("thresh", thresholded)
+    cv.imshow("graph", blank)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
